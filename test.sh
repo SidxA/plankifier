7beb1d5085d470ab7791e9d3e3829499c145140f
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-05 13:21:57 +0100

    first night test

--- test.sh
+++ test.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 
-python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=False -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=False -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
 
-python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='brightness' -augparameter=0.1 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='h_flip' -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
 
-python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='rescale' -augparameter=1.5 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='v_flip' -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=10 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=20 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=30 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
 
 wait
 
-python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=40 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
 
-python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='brightness' -augparameter=0.3 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=50 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
 
-python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='rescale' -augparameter=15 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=60 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=70 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=80 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+
+python3 augment.py -totEpochs=40 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=90 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
