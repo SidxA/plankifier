bb134d844891c1825ea45baf1109466662fc3509
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-05 09:09:33 +0100

    aa

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {0..40..10}
+do
+python3 augment.py -totEpochs=50 -width=128 -height=128 -model=conv2 -aug=True -augtype='rotate' -augparameter=$i -resize=keep_proportions -bs=8 -lr=0.0001 -verbose=False -opt=sgd -datapath='./data/'
+done
