13d694b2682dff0ea3b479ab6071f11ae37fae41
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-05 09:57:31 +0100

    x

--- test.sh
+++ test.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 
-A=python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='rotate' -augparameter=$360 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
-	echo $L >> A.txt
+python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='rotate' -augparameter=$360 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' & >> A.txt
 
-B=python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=45 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
-	echo $L >> B.txt
+python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=45 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' & >> B.txt
 
-C=python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=90 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
-	echo $L >> C.txt
+python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=90 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' & >> C.txt
