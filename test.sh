b4e57126de524fe54249d01e0c1f0c9bf1b71ac1
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-05 09:52:10 +0100

    s

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for i in {0..40..10}
-do
-python3 augment.py -totEpochs=50 -width=128 -height=128 -model=conv2 -aug=True -augtype='rotate' -augparameter=$i -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/'
-done
+
+A = python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='rotate' -augparameter=$360 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+	echo $L >> A.txt
+
+B = python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=45 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+	echo $L >> B.txt
+
+C = python3 augment.py -totEpochs=1 -width=128 -height=128 -model=conv2 -aug=True -augtype='shear' -augparameter=90 -resize=keep_proportions -bs=8 -lr=0.0001 -opt=sgd -datapath='./data/' &
+	echo $L >> C.txt
