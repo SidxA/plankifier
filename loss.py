99dbd3da811d41eadad7ebc9ca9b3fb431be6cb8
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-09 15:35:14 +0100

    yes

--- loss.py
+++ loss.py
@@ -193,7 +193,7 @@ else:
 	model.compile(loss=args.loss, optimizer=opt, metrics=["accuracy"])
 
 # checkpoints
-checkpointer    = keras.callbacks.ModelCheckpoint(filepath=outDir+'/bestweights.hdf5', monitor='val_loss', verbose=0, save_best_only=True) # save the model at every epoch in which there is an improvement in test accuracy
+checkpointer    = keras.callbacks.ModelCheckpoint(filepath=outDir+'/bestweights.hdf5', monitor='val_loss', verbose=2, save_best_only=True) # save the model at every epoch in which there is an improvement in test accuracy
 # coitointerrotto = keras.callbacks.callbacks.EarlyStopping(monitor='val_loss', patience=args.totEpochs, restore_best_weights=True)
 logger          = keras.callbacks.callbacks.CSVLogger(outDir+'epochs.log', separator=' ', append=False)
 callbacks=[checkpointer, logger]
@@ -210,7 +210,7 @@ if args.aug:
 		epochs=args.totEpochs, 
 		callbacks=callbacks,
 		initial_epoch = args.initial_epoch,
-		verbose = 0)
+		verbose = 2)
 else:
 	history = model.fit(
 		trainX, trainY, batch_size=args.bs, 
