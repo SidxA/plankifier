3baf39e3892f698ee0dacc3493a56638c384c596
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-09 15:51:25 +0100

    y

--- loss.py
+++ loss.py
@@ -21,6 +21,10 @@ from PIL import Image
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+import tensorflow as tf
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 parser = argparse.ArgumentParser(description='Train a model on zooplankton images')
 parser.add_argument('-datapath', default='./data/2019.11.20_zooplankton_trainingset_TOM/', help="Print many messages on screen.")
