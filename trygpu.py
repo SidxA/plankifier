9f304b7f984216ac396dfaf1896e4fb06c7cffcf
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-12 11:21:07 +0100

    x

--- trygpu.py
+++ trygpu.py
@@ -27,13 +27,13 @@ import tensorflow as tf
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
-try:
-    for gpu in gpus:
-        tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-except RuntimeError as e:
-    print(e) 
+	try:
+		for gpu in gpus:
+			tf.config.experimental.set_memory_growth(gpu, True)
+			logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+			print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+	except RuntimeError as e:
+		print(e) 
 
 
 parser = argparse.ArgumentParser(description='Train a model on zooplankton images')
