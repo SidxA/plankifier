a5a639d83bc35aa484fad736b400d0594f911b6a
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-18 12:48:44 +0100

    x

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 ##!/bin/bash
 
-python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=True -resize=keep_proportions -bs=8 -lr=0.0001 -opt='adam_2' -datapath='./data/' -loss='categorical_crossentropy' &
-python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=False -resize=keep_proportions -bs=8 -lr=0.0001 -opt='adam_2' -datapath='./data/' -loss='categorical_crossentropy' &
+python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=True -resize=keep_proportions -bs=8 -lr=0.0001 -opt='adam_2' -datapath='./data/' &
+python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=False -resize=keep_proportions -bs=8 -lr=0.0001 -opt='adam_2' -datapath='./data/' &
 
 wait
 
-python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=True -resize=keep_proportions -bs=8 -lr=0.0001 -opt='sgd_2' -datapath='./data/' -loss='categorical_crossentropy' &
-python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=False -resize=keep_proportions -bs=8 -lr=0.0001 -opt='sgd_2' -datapath='./data/' -loss='categorical_crossentropy' &
+python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=True -resize=keep_proportions -bs=8 -lr=0.0001 -opt='sgd_2' -datapath='./data/' &
+python3 optimizer.py -totEpochs=100 -width=128 -height=128 -model=conv2 -aug=False -resize=keep_proportions -bs=8 -lr=0.0001 -opt='sgd_2' -datapath='./data/' &
