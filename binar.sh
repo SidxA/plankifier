5a7a4bfab26e71f76a7fabb358d5a5357a41a019
Leonard Schulz <leonard.schulz@campus.tu-berlin.de>
2020-03-20 13:15:29 +0100

    x

--- binar.sh
+++ binar.sh
@@ -2,5 +2,5 @@
 
 for i in {64..256..64}
 	do
-	python3 binary.py -limit=100 -totEpochs=10 -number1=$i -number2=$i -number3=$i opt='rmsprop'
+	python3 binary.py -limit=100 -totEpochs=10 -opt='rmsprop' -number1=$i -number2=$i -number3=$i 
 	done
